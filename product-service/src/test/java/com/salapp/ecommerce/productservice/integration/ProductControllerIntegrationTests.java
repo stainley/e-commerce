d5b347a003dfeceec2a8a0b4968d9589e4217add
Stainley Lebron <saiyamans@gmail.com>
2022-06-28 20:10:29 -0400

    EC-20 Fixed update

--- product-service/src/test/java/com/salapp/ecommerce/productservice/integration/ProductControllerIntegrationTests.java
+++ product-service/src/test/java/com/salapp/ecommerce/productservice/integration/ProductControllerIntegrationTests.java
@@ -3,6 +3,7 @@ package com.salapp.ecommerce.productservice.integration;
 import com.salapp.ecommerce.productservice.entity.ProductEntity;
 import com.salapp.ecommerce.productservice.repository.IProductRepository;
 import com.salapp.ecommerce.productservice.services.IProductService;
+import org.checkerframework.checker.units.qual.A;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,9 +11,12 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.webservices.server.AutoConfigureMockWebServiceClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,11 +24,14 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Optional;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductControllerIntegrationTests {
 
-    private static final Long PRODUCT_ID = 1L;
+        private static final Long PRODUCT_ID = 1L;
 
     @Autowired
     private IProductService productService;
@@ -42,6 +49,7 @@ public class ProductControllerIntegrationTests {
     void loadContext() {
         Assertions.assertNotNull(restTemplate);
         Assertions.assertNotNull(productService);
+        Assertions.assertNotNull(mockMvc);
     }
 
     @Test
@@ -52,6 +60,9 @@ public class ProductControllerIntegrationTests {
 
         mockMvc.perform(MockMvcRequestBuilders.delete("http://localhost:8070/api/v1/product/" + PRODUCT_ID)
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("product " + PRODUCT_ID + " has been deleted!")));
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.apiError.status", Matchers.is("NO_CONTENT")));
+                /*.andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("product " + PRODUCT_ID + " has been deleted!")));*/
     }
 }
